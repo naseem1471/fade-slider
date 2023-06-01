829b24ac614a9a17a8b190100e63db7efbdee7d9
Naseem Akbar <101437439+naseem1471@users.noreply.github.com>
2023-06-01 06:29:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+    (function(){
+            "use strict";
+   
+                var currentImage = 0;
+                var myphotos = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg"];
+                var container = document.getElementById("content");
+                var nextBtn = document.getElementById("next");
+                var prevBtn = document.getElementById("previous");
+      
+                nextBtn.addEventListener("click", function(event) {
+                        event.preventDefault();
+               
+                        currentImage++;
+                        if( currentImage > myphotos.length-1 ) {currentImage = 0};
+                        swap();
+                    });
+
+                    prevBtn.addEventListener("click", function(event) {
+                        event.preventDefault();
+                    
+                        currentImage--;
+                        if (currentImage < 0) {currentImage = myphotos.length -1 };
+                        swap();
+                    });
+
+                        function swap(){
+                        var newSlide = document.createElement("img");
+                        newSlide.src = `slides/${myphotos[currentImage]}`;
+                        newSlide.className = 'fadeinimg';
+                        container.appendChild(newSlide);
+                        if(container.children.length > 2) { container.removeChild(container.children[0])};
+                        }
+
+                
+                    })();            
